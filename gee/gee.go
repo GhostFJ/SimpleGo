1a740a8756647bfb900d74b949a04f6cf1216381
GhostFJ <18395589072@163.com>
2021-12-19 11:18:43 +0800

    day2

--- gee/gee.go
+++ gee/gee.go
@@ -1,20 +1,51 @@
 package gee
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // HandlerFunc 定义请求处理程序
-type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+type HandlerFunc func(c *Context)
 
 // Engine Engine需要实现ServeHTTP接口
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router // 路由映射表
+}
+
+// 实现ServeHTTP接口
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request)  {
+	//key := req.Method + "-" + req.URL.Path
+	//if hander, ok := engine.router[key]; ok {
+	//	hander(w, req)
+	//} else {
+	//	w.WriteHeader(http.StatusNotFound)
+	//	fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	//}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
 
 // New 构造函数
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
-func ()  {
-	
+// 路由表
+func (engine *Engine) addRoute(method string, pattern string, hander HandlerFunc)  {
+	engine.router.addRoute(method, pattern, hander)
+}
+
+// GET 添加GET请求
+func (engine *Engine) GET(pattern string, handler HandlerFunc)  {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST 添加POST请求
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run 启动函数 ListenAndServe 的包装
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
 }
--- go.mod
+++ go.mod
@@ -2,8 +2,6 @@ module example
 
 go 1.15
 
-require gee v0.0.0
+replace gee => ./gee
 
-replace (
-	gee => ./gee
-)
+require gee v0.0.0-00010101000000-000000000000
